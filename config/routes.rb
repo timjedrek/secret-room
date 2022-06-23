07385f1e4eedadadd34ecd7abc80147c34fe79b1
Tim Jedrek <tim@timjedrek.com>
2022-06-22 22:08:30 -0500

    try to fix auth by changing route and adding tests by including error messages and if user logged in change buttons

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     get "users", to: "devise/sessions#new"
   end
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :secrets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
