815ca2487157e81045b0c436d849ace113e12ad0
Tim Jedrek <tim@timjedrek.com>
2022-07-02 13:45:29 -0500

    fix usersecret page route by changing order

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,9 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :secrets
 
   get "/secrets/usersecrets" => "secrets#usersecrets"
+  resources :secrets
 
   # static pages
   get "/pages/:page" => "pages#show"
